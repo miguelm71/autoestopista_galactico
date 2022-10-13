0089d83db1183b78cb8da56643435f808ed9775b
miguelm71 <mangel.mateo@gmail.com>
2022-10-13 16:38:59 +0200

    test

--- ft_printf/test.c
+++ ft_printf/test.c
@@ -8,41 +8,48 @@ int	mi_print(char *ss, ...)
 {
 	char	*str;
 	va_list	ptr;
+	char *param_string;
+	int param_integer;
 
-	va_start(ptr, ss);
+	va_start(ptr, ss); //inicializa el puntero a la lista de parametros.
 	str = ss;
 	while (*str)
 	{
-		if (isalpha(*str))
+		if (isprint(*str))
 		{
 			if (*str == '%')
 			{
-				printf("%% detected\n");
+				printf("\n%% detected\n");
 				str++;
 				if (*str == 'i')
-					printf("i detected: %d\n", va_arg(ptr, int));
+				{
+					 //coge el primer parametro
+					param_integer = va_arg(ptr, int);
+					printf("i detected: %d\n", param_integer);
+				}
 				else if (*str == 'a')
-					printf ("a detected: %s\n", va_arg(ptr, char*));
+				{
+					//coge el segundo parametro
+					param_string = va_arg(ptr, char *);
+					printf ("a detected: %s\n", param_string);
+				}
 				else
 					write(1, str, 1);
 			}
 			else
 				write(1, str, 1);
-			
 		}
 		str++;
 	}
+	//finaliza el puntero a la lista de parametros
 	va_end(ptr);
-	write(1, "\0", 1);
+	write(1, "\n", 1);
 	return (0);
 }
 
-
-
 int	main(int argn, char **argv)
 {
-	char	ss[] = "hola ca%iracola.";
+	char	ss[] = "hola ca%ira%acola.";
 
 	mi_print(ss, 34, "aaa");
 }
-
