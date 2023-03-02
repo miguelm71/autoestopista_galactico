a034f3b4749222c16974c5a4eb22c1b1545bd6db
Miguel Angel Mateo Montero <mangel.mateo@gmail.com>
2023-03-02 17:25:46 +0100

    error %c caracter nulo en no bonus fixed

--- ft_printf2/ft_printer4.c
+++ ft_printf2/ft_printer4.c
@@ -6,7 +6,7 @@
 /*   By: mmateo-m <mmateo-m@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 15:19:47 by mmateo-m          #+#    #+#             */
-/*   Updated: 2023/02/22 08:27:45 by mmateo-m         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:31:45 by mmateo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,18 @@ int	ft_write_string(t_tp *tp, char *str, char t)
 	n = 0;
 	tp->cap_hex = 0;
 	i = ft_strlen(str);
-	while (n < i && t != 0)
+	if (t == 'c' && *str == 0)
 	{
-		write(1, &(str[n]), 1);
-		n++;
+		write (1, "\0", 1);
+		i = 1;
+	}
+	else
+	{
+		while (n < i && t != 0)
+		{
+			write(1, &(str[n]), 1);
+			n++;
+		}
 	}
 	free(tp);
 	return (i);
--- ft_printf2/test.c
+++ ft_printf2/test.c
@@ -6,7 +6,7 @@
 /*   By: mmateo-m <mmateo-m@student.42madrid.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 12:47:17 by mmateo-m          #+#    #+#             */
-/*   Updated: 2023/02/22 16:09:02 by mmateo-m         ###   ########.fr       */
+/*   Updated: 2023/03/02 16:38:53 by mmateo-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,9 @@ int	main(void)
 	int	m;
 
 	printf ("printf:\n");
-	n = printf(";p]1/^(MN%xO`]0%c\r3\r(+:t(\v{%pNH`G*%ij", -1013443380, 840100864, (void *)-4904886001540910798, 151131924);
-	printf ("]");
+	n = printf("h%c%%oz37", 1454300672);
 	printf ("\nft_printf:\n");
-	m = ft_printf(";p]1/^(MN%xO`]0%c\r3\r(+:t(\v{%pNH`G*%ij", -1013443380, 840100864, (void *)-4904886001540910798, 151131924);
+	m = ft_printf("h%c%%oz37", 1454300672);
 	printf ("]\n");
 	printf ("p_n=%d     ft_p_m=%d", n, m);
 	return (1);
